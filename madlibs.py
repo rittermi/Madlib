c065ab5607549df08dca653375dedf8d173c2f13
Marisela Allen <martiallen@Mariselas-MacBook-Pro.local>
2019-09-18 19:41:57 -0400

    git add*

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#%%
+import random
+#%%
+verbs = ["hi","hello","marti"]
+print(verbs)
+#%%
+verbs = ["stalked","watched","followed"]
+print("The killer " + random.choice(verbs) + " the woman all night")
+#%%
+verbs = ["ran", "screamed", "fought"]
+print("She noticed and the woman " + random.choice(verbs) + " for her life away from the killer")
+
+#%%
+
+
+#%%
